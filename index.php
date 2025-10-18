c07f1d7ab2ef60cc8862d1dfe0516e1a6d37926f
iZall. <radityafd07@gmail.com>
2025-10-18 22:56:47 +0700

    fix fitur: detail mobil

--- index.php
+++ index.php
@@ -17,11 +17,16 @@ if (file_exists($controllerFile)) {
     $controllerObj = new $controllerClass($conn);
 
     if (method_exists($controllerObj, $action)) {
-        $controllerObj->$action();
+        if (isset($_GET['id'])) {
+            // kalau ada parameter id, kirimkan ke method
+            $controllerObj->$action($_GET['id']);
+        } else {
+            // kalau tidak, panggil tanpa argumen
+            $controllerObj->$action();
+        }
     } else {
         echo "Action '$action' tidak ditemukan di controller '$controllerClass'.";
     }
 } else {
     echo "Controller '$controller' tidak ditemukan.";
 }
-?>
